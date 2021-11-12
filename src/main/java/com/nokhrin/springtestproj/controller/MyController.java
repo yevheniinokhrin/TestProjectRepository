974b9ecb7c215582f54f79b642604da14ecddeb6
yevheniinokhrin <233315@edu.p.lodz.pl>
2021-11-12 22:23:14 +0100

    second commit

--- src/main/java/com/nokhrin/springtestproj/controller/MyController.java
+++ src/main/java/com/nokhrin/springtestproj/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.nokhrin.springtestproj.controller;
 
 
+import com.nokhrin.springtestproj.model.MyUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,5 +14,14 @@ public class MyController {
     @GetMapping("/fck/{userName}")
     public String firstMethod(@PathVariable String userName){
         return "Fuck "+userName;
+    }
+    @GetMapping("/getUser")
+    public MyUser secondMethod(){
+        MyUser myUser = new MyUser();
+        myUser.setAge(19);
+        myUser.setEmail("312312");
+        myUser.setFirstName("John");
+        myUser.setLastName("Cena");
+        return myUser;
     }
 }
---
+++ src/main/java/com/nokhrin/springtestproj/model/MyUser.java
@@ -0,0 +1,49 @@
+package com.nokhrin.springtestproj.model;
+
+public class MyUser {
+    private long id;
+    String firstName;
+    String lastName;
+    String email;
+    int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
