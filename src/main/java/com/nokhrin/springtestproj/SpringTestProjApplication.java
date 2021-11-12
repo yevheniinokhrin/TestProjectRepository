27be999d7a6838bb375f0a33e6cbeaaa7927a302
yevheniinokhrin <233315@edu.p.lodz.pl>
2021-11-12 21:39:08 +0100

    first commit

---
+++ src/main/java/com/nokhrin/springtestproj/SpringTestProjApplication.java
@@ -0,0 +1,13 @@
+package com.nokhrin.springtestproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nokhrin/springtestproj/SpringTestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokhrin.springtestproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
